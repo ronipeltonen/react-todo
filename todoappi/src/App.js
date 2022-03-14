ac26b63c6c3517fd215245b98c1d3c4b2bd2fb9b
Tuomas Suutari <tuomas@nepnep.net>
2022-03-14 15:06:24 +0200

    Hae tehtavat API:sta Axiosilla

--- todoappi/src/App.js
+++ todoappi/src/App.js
@@ -1,17 +1,28 @@
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 import TodoLista from './TodoLista';
 
-function App() {
-  const data = [
-    {"otsikko": "Ykkönen"},
-    {"otsikko": "Kakkonen"},
-    {"otsikko": "Kolmonen"},
-  ];
-  return (
-    <div className="App">
-      <TodoLista iteemit={data}/>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  state = {
+    iteemit: []
+  }
 
-export default App;
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/api/tehtavat/`)
+      .then(res => {
+        const iteemit = res.data;
+        this.setState({iteemit});
+      })
+  }
+
+  render() {
+    const data = this.state.iteemit;
+    console.log(data);
+    return (
+      <div className="App">
+        <TodoLista iteemit={data}/>
+      </div>
+    );
+  }
+}
